a6c4788b590c5a61d70584a817e9b9f5eb6e6aa1
Olufemi Awe <femi@gitpod.io>
2024-05-30 14:38:47 +0000

    code genesis

---
+++ .gitpod.Dockerfile
@@ -0,0 +1,5 @@
+# .gitpod.Dockerfile
+FROM gitpod/workspace-python-3.10
+
+# Install Node.js and npm
+RUN brew install node
---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(`${req.method} request for '${req.url}'`);
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.get('/about', (req, res) => {
+  res.send('About Page');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
