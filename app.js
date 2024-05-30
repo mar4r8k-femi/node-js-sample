4f8ac04e9630aee75dde33d9f467d785f08e65eb
Olufemi Awe <femi@gitpod.io>
2024-05-30 11:06:49 -0400

    Update app.js

--- app.js
+++ app.js
@@ -16,5 +16,5 @@ app.get('/about', (req, res) => {
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
